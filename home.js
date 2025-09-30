e86c095d20f415399d43667e3f5072f89bb98839
sajid <shajiedrahman@gmail.com>
2025-09-30 16:18:52 +0600

    added validation for accountNumber and pin number

--- home.js
+++ home.js
@@ -1,13 +1,21 @@
+const pin=3333;
 document.getElementById("login-btn").addEventListener('click',function(e){
     e.preventDefault()
     // console.log("is working!");
     const bank=document.getElementById('add-bank').value ;
     const accountNumber=document.getElementById('add-account-number').value ;
     const ammount=parseInt(document.getElementById('add-ammount').value) ;
-    const pin=document.getElementById('add-pin').value ;
+    const userPin=parseInt(document.getElementById('add-pin').value) ;
     // console.log(bank,accountNumber,ammount,pin);
     const availabeMoney=parseInt(document.getElementById('availabe-money').innerText);
-
+    if(accountNumber.length!=11){
+        alert("please provide valid account Number");
+        return;
+    }
+    if(pin!==userPin ){
+        alert("Please provide valid pin number");
+        return;
+    }
     // console.log(availabeMoney);
     const totalMoney=ammount+availabeMoney;
 
